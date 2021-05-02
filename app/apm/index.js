550757d55ffce549f82459dced1221cad15b4a6f
John Watson <johnwatson484@hotmail.com>
2021-05-02 16:48:38 +0100

    Fix agent

--- app/apm/index.js
+++ app/apm/index.js
@@ -1,4 +1,4 @@
-const agent = require('skywalking-backend-js')
+const { agent } = require('skywalking-backend-js')
 
 function startMonitoring () {
   if (process.env.SKYWALKING_COLLECTOR_ADDRESS) {

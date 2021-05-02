7dd0f41fc8edbc570f7b7d2da4a33fab0ef32010
John Watson <johnwatson484@hotmail.com>
2021-05-02 16:56:57 +0100

    Fix agent

--- app/apm/index.js
+++ app/apm/index.js
@@ -1,8 +1,8 @@
-const { agent } = require('skywalking-backend-js')
+const agent = require('skywalking-backend-js')
 
 function startMonitoring () {
   if (process.env.SKYWALKING_COLLECTOR_ADDRESS) {
-    agent.start({
+    agent.default.start({
       serviceName: process.env.SKYWALKING_SERVICE_NAME,
       collectorAddress: process.env.SKYWALKING_COLLECTOR_ADDRESS
     })
